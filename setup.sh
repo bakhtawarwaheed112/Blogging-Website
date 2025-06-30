d3f4ad69b83ca4165fb941913072be8aae058ba8
l1s22bsse0084 <l1s22bsse0084@ucp.edu.pk>
2025-06-30 14:25:50 +0500

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git init 
+git config --local user.name "l1s22bsse0084"
+git config --local user.email "l1s22bsse0084@ucp.edu.pk"
+git config --local core.editor notepad
+git branch -m develop
+mkdir ./src ./styles ./asssts ./bn ./modules
+touch ./src/index.html ./styles/index.css
+touch .gitignore
+git remote add origin https://github.com/bakhtawarwaheed112/Blogging-Website.git
